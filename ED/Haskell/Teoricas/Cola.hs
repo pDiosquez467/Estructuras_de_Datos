2e7731f771db801bf6a88fde5b965417bf84e027
pdiosquez467 <pablodiosquez467@gmail.com>
2025-12-29 20:16:10 -0300

    Agrego variantes de impl para el TDA Cola

--- ED/Haskell/Teoricas/Cola.hs
+++ ED/Haskell/Teoricas/Cola.hs
@@ -1,133 +1,110 @@
-module ED.Haskell.Teoricas.Cola where
+module Cola 
+    ( Cola
+    , vaciaC       -- :: Cola a
+    , estaVaciaC   -- :: Cola a -> Bool
+    , encolarC     -- :: a -> Cola a -> Cola a
+    , proximoC     -- :: Cola a -> a
+    , desencolarC  -- :: Cola a -> Cola a
+    ) where
 
----- Interfaz del módulo Cola
+-------------------------------------------------------------------------------
+-- INTERFAZ
+-------------------------------------------------------------------------------
+
+-- | Tipo abstracto Cola (FIFO).
+data Cola a = MkC [a] [a] deriving Show
 
 vaciaC      :: Cola a
 estaVaciaC  :: Cola a -> Bool
 encolarC    :: a -> Cola a -> Cola a
 
--- Pre: La cola no debe estar vacía. 
---
+-- | Devuelve el próximo elemento (el primero en haber entrado).
+-- Pre: La cola no debe estar vacía.
 proximoC    :: Cola a -> a
 
--- Pre: La cola no debe estar vacía. 
---
+-- | Elimina el primer elemento de la cola.
+-- Pre: La cola no debe estar vacía.
 desencolarC :: Cola a -> Cola a
 
----- Implementación del módulo Cola 
-----------------------------------------------------------------
--- Variante 1: el próximo elemento elemento se encuentra al principio
--- de la lista. 
---
+-------------------------------------------------------------------------------
+-- IMPLEMENTACIÓN (Variante 3: Dos Listas)
+-------------------------------------------------------------------------------
+-- Estructura: MkC front back
+-- Invariante: Los elementos se encolan en 'back' y se sacan de 'front'.
+-- Cuando 'front' se vacía, se invierte 'back' y se pasa a 'front'.
 
--- estaVaciaC: es O(1).
-
--- encolarC: es lineal, o sea O(n) para una cola de n elementos. 
-
--- proximoC: es O(1).
-
--- desencolarC: es O(1).
-
--- data Cola a = MkC [a]
-
--- vaciaC               = MkC [] 
-
--- estaVaciaC (MkC xs)  = null xs
-
--- encolarC x (MkC xs)  = MkC (xs ++ [x])
-
--- proximoC (MkC xs)    = head xs 
-
--- desencolarC (MkC xs) = MkC (tail xs)
-
-----------------------------------------------------------------
--- Variante 2: El próximo elemento se encuentra al final de la lista. 
---
-
--- estaVaciaC: es O(1).
-
--- encolarC: es O(1). 
-
--- proximoC: es O(n) siendo n la cantidad de elementos de la cola.
-
--- desencolarC: es O(n), siendo n la cantidad de elementos de la cola.
-
--- data Cola a = MkC [a]
-
--- vaciaC = MkC []
-
--- estaVaciaC (MkC xs)  = null xs 
-
--- encolarC x (MkC xs)  = MkC (x : xs)
-
--- proximoC (MkC xs)    = last' xs         -- last: es lineal. 
-
--- desencolarC (MkC xs) = MkC (init' xs)   -- init: es lineal. 
-
--- -- Propósito: 
--- -- Devuelve el último elemento de la lista. 
--- -- Pre: La lista no debe ser vacía.
--- -- Complejidad: O(n), pues debe recorrer toda la lista hasta 
--- -- alcanzar el último.
--- --
--- last' :: [a] -> a 
--- last' (x : xs) = 
---     if null xs 
---         then x 
---         else last' xs
-
--- -- Propósito: 
--- -- Devuelve una nueva lista con todos los elementos de la lista dada 
--- -- excepto el último. 
--- -- Pre: La lista no debe ser vacía.
--- -- Complejidad: O(n), pues debe recorrer toda la lista hasta 
--- -- alcanzar el último.
--- --
--- init' :: [a] -> [a] 
--- init' (x : xs) = 
---     if null xs 
---         then []
---         else init' xs 
-
-----------------------------------------------------------------
--- Variante 3: usando dos listas => front y back 
---
-
--- estaVaciaC: es O(1).
-
--- encolarC: es O(1). 
-
--- proximoC y desencolarC son O(1) amortizado. 
-
--- Ups :)
-
-data Cola a = MkC [a] [a]
-
+-- Costo: O(1)
 vaciaC = MkC [] []
 
-estaVaciaC (MkC fs xs)  = null fs && null xs
+-- Costo: O(1)
+estaVaciaC (MkC fs bs) = null fs && null bs
 
-encolarC x (MkC fs xs)  = MkC fs (x : xs)
+-- Costo: O(1)
+encolarC x (MkC fs bs) = MkC fs (x : bs)
 
-proximoC (MkC fs xs)    =
+-- Costo: O(1) si fs no es vacía. O(n) en el peor caso (si fs es vacía).
+proximoC (MkC fs bs) =
     if null fs
-        then last xs  -- O(n)
-        else head fs  -- O(1) 
+        then last bs 
+        else head fs 
 
-desencolarC (MkC fs xs) =
+-- Costo: O(1) amortizado.
+desencolarC (MkC fs bs) =
     if null fs
-        then MkC (tail (reverse' xs)) []  -- O(n)
-        else MkC (tail fs) xs            -- O(1) 
+        then MkC (tail (reverse' bs)) []  -- O(n) al invertir, pero ocurre poco.
+        else MkC (tail fs) bs             -- O(1)
 
--- reverse en O(n)
---
+-------------------------------------------------------------------------------
+-- AUXILIARES
+-------------------------------------------------------------------------------
 
--- Complejidad: O(n), siendo n la longitud de la lista. 
---
+-- Propósito: Invierte una lista.
+-- Complejidad: O(n).
 reverse' :: [a] -> [a]
 reverse' xs = reverse'' xs []
 
 reverse'' :: [a] -> [a] -> [a]
--- reverse'' [] ys = ys 
--- reverse'' (x : xs) ys = reverse'' xs (x : ys)
 reverse'' xs ys = foldl (flip (:)) ys xs
+
+-------------------------------------------------------------------------------
+-- OTRAS VARIANTES (NO UTILIZADAS)
+-------------------------------------------------------------------------------
+
+{- 
+-- Variante 1: El próximo elemento está al PRINCIPIO de la lista.
+-- Costos: encolar O(n), resto O(1).
+
+data Cola a = MkC [a]
+
+vaciaC               = MkC [] 
+estaVaciaC (MkC xs)  = null xs
+encolarC x (MkC xs)  = MkC (xs ++ [x])
+proximoC (MkC xs)    = head xs 
+desencolarC (MkC xs) = MkC (tail xs)
+-}
+
+{- 
+-- Variante 2: El próximo elemento está al FINAL de la lista.
+-- Costos: encolar O(1), proximo/desencolar O(n).
+
+data Cola a = MkC [a]
+
+vaciaC               = MkC []
+estaVaciaC (MkC xs)  = null xs 
+encolarC x (MkC xs)  = MkC (x : xs)
+proximoC (MkC xs)    = last' xs         
+desencolarC (MkC xs) = MkC (init' xs)   
+
+-- Auxiliar: last'
+-- Complejidad: O(n).
+last' :: [a] -> a 
+last' (x : xs) = if null xs then x else last' xs
+
+-- Auxiliar: init'
+-- Complejidad: O(n).
+init' :: [a] -> [a] 
+init' (x : xs) = 
+    if null xs 
+        then []
+        else x : init' xs  -- Corrección: se debe conservar x.
+-}
