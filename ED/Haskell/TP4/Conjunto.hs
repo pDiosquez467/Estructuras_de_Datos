48306a32989af5da4cabbfed04bfc8f2a2d5a39c
pDiosquez467 <pabloDiosquez467@gmail.com>
2025-12-30 10:51:06 -0300

    Agrego implementación del módulo 'Conjunto'

--- ED/Haskell/TP4/Conjunto.hs
+++ ED/Haskell/TP4/Conjunto.hs
@@ -1,7 +1,6 @@
-module Conjunto where
+module Conjunto (Conjunto, vacioC, agregarC, perteneceC, cantidadC, borrarC, unionC, listaC) where
 
-
--- Interfaz del módulo Conjunto
+---- Interfaz del módulo Conjunto
 --
 
 -- Crea un conjunto vacío.
@@ -42,3 +41,54 @@ unionC :: Eq a -> Conjunto a -> Conjunto a -> Conjunto a
 -- Dado un conjunto devuelve una lista con todos los elementos
 -- distintos del conjunto.
 listaC :: Eq a => Conjunto a -> [a]
+
+---- Implementación del módulo Conjunto
+--
+
+data Conjunto a = MkC [a] deriving Show
+
+vacioC :: Conjunto a
+vacioC = MkC []
+
+agregarC :: Eq a => a -> Conjunto a -> Conjunto a
+agregarC x (MkC xs) = 
+    if elem x xs
+        then MkC xs 
+        else MkC (x : xs)
+
+perteneceC :: Eq a => a -> Conjunto a -> Bool
+perteneceC x (MkC xs) = elem x xs
+
+cantidadC :: Eq a => Conjunto a -> Int
+cantidadC (MkC xs) = length xs
+
+borrarC :: Eq a => a -> Conjunto a -> Conjunto a
+borrarC x (MkC xs) =
+    if elem x xs
+        then MkC (borrar x xs)
+        else MkC xs
+
+-- Función auxiliar privada
+--
+borrar :: Eq a => a -> [a] -> [a]
+borrar _ [] = []
+borrar e (x : xs) =
+    if e == x
+        then xs
+        else x : borrar e xs
+
+unionC :: Eq a => Conjunto a -> Conjunto a -> Conjunto a
+unionC (MkC xs) (MkC ys) =
+    MkC (agregarSinDuplicados xs ys)
+
+-- Función auxiliar privada
+--
+agregarSinDuplicados :: Eq a => [a] -> [a] -> [a]
+agregarSinDuplicados [] ys       = ys
+agregarSinDuplicados (x : xs) ys =
+    if elem x ys
+        then agregarSinDuplicados xs ys
+        else agregarSinDuplicados xs (x : ys)
+
+listaC :: Eq a => Conjunto a -> [a]
+listaC (MkC xs) = xs
